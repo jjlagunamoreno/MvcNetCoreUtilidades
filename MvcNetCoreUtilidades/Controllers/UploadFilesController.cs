5f7c2b50b60f8a65261b10cefa1f34b31d03f1f1
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-02-20 10:59:44 +0100

    Subir imagenes al servidor y manejarlas.

---
+++ MvcNetCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCoreUtilidades.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        private IWebHostEnvironment hostEnvironment;
+
+        public UploadFilesController(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult SubirFichero()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>
+            SubirFichero(IFormFile fichero)
+        {
+            //NECESITAMOS LA RUTA A NUESTRO WWWROOT DEL SERVER
+            string rootFolder =
+                this.hostEnvironment.WebRootPath;
+            //COMENZAMOS ALMACENANDO EL FICHERO EN LOS 
+            //ELEMENTOS TEMPORALES
+            string fileName = fichero.FileName;
+            //LAS RUTAS DE FICHEROS NO DEBO ESCRIBIRLAS, TENGO QUE GENERAR
+            //DICHAS RUTAS CON EL SISTEMA DONDE ESTOY TRABAJANDO
+            string path = Path.Combine(rootFolder, "uploads", fileName);
+            //PARA SUBIR EL FICHERO SE UTILIZA Stream CON IFormFile
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await fichero.CopyToAsync(stream);
+            }
+            ViewData["MENSAJE"] = "Fichero subido a " + path;
+            return View();
+        }
+    }
+}
